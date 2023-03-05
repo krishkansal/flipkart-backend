e24842a61e2fcbc18631cb0e1342f0ef7a325f8a
krishkansal <krishkansal7@gmail.com>
2023-03-05 14:43:32 +0530

    cart

--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { userSignup } from '../controller/user-controller.js';
 import { userLogin } from '../controller/user-controller.js';
-import { getProducts } from '../controller/product-controller.js';
+import { getProducts ,getProductById} from '../controller/product-controller.js';
 
 const router=express.Router();
 // router.get('/',(req,res)=>{
@@ -12,6 +12,7 @@ router.post('/signup',userSignup);
 router.post('/login',userLogin);
 
 router.get('/products',getProducts);
+router.get('/product/:id', getProductById);
 
 // router.post('/signup',(request,response)=>{
 //     try{
